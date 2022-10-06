ed82b3b69160123620e39a3e4325ff4ac3ea596d
Scott Holden <scott@nullops.io>
2022-10-07 10:53:00 +1100

    Added demo3 events

--- Demo3/src/Demo3/Program.cs
+++ Demo3/src/Demo3/Program.cs
@@ -1,8 +1,33 @@
-﻿using Demo;
+﻿using System.Text.Json;
+using Azure.Messaging.ServiceBus;
+using Azure.Core;
+using Azure.Identity;
+using Demo;
+
+string serviceBusNamespace = GetEnvironmentVariable("sb-namespace");
+string serviceBusTopic = GetEnvironmentVariable("sb-topic");
+
+#if DEBUG
+TokenCredential credential = new DefaultAzureCredential();
+#else
+TokenCredential credential = new ManagedIdentityCredential();
+#endif
+
+ServiceBusClient sbc = new(serviceBusNamespace, credential);
+ServiceBusSender sender = sbc.CreateSender(serviceBusTopic);
+
 new Frontend().Run(args, AddressChangeRequest);
 
 // In demo 3, we publish an event to a Service Bus Topic
 async Task<string> AddressChangeRequest(AddressChange change)
 {
-	return "Ok";
+	await sender.SendMessageAsync(new ServiceBusMessage(JsonSerializer.Serialize(change)));
+	return "Address successfully updated.";
+}
+
+static string GetEnvironmentVariable(string name)
+{
+	string? value = Environment.GetEnvironmentVariable(name);
+	if (!string.IsNullOrWhiteSpace(value)) return value;
+	throw new Exception("Unable to read env var " + name);
 }
